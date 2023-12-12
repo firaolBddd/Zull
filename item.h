3495a16c9490ecd7949c8f9055ff68392c1bcb1c
firaolBddd <144168336+firaolBddd@users.noreply.github.com>
2023-12-11 18:11:38 -0800

    Create item.h

---
+++ item.h
@@ -0,0 +1,18 @@
+//header file for items 
+//asign public var
+#include <iostream>
+#include <cstring>
+
+#ifndef ADD_H
+#define ADD_H
+
+using namespace std;
+
+class Item{
+public:
+  void setName(char* nameOut);
+  void getName(char* nameOut);
+  void printItem();
+  char name[80];
+};
+#endif
